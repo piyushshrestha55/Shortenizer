0bd47c9201399c1790c2eefb2e8b6c23a32e0040
piyush55 <snatch.sth@gmail.com>
2026-04-25 12:53:35 +0545

    Creating the NavBar

--- app/page.js
+++ app/page.js
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section>
+        <div>
+          <p>The best URL Shortenizer that you can get in the market</p>
+          <p>We are most simplified and easy to use URL Shortenizer</p>
+        </div>
+      </section>
+    </main>
+  );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,37 @@
 import React from "react";
-
+import Link from "next/link";
 const Navbar = () => {
-  return <div>I am a NavBar</div>;
+  return (
+    <nav className="bg-purple-800  text-white h-16 px-3 flex justify-between items-center">
+      <div className="logo font-bold">Shortenizer</div>
+      <ul className=" flex gap-4 justify-center items-center">
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/generate">
+          <li>Shorten</li>
+        </Link>
+        <Link href="/contact">
+          <li>Contact</li>
+        </Link>
+        <li className="flex gap-3">
+          <Link href="/generate">
+            <button className="text-white bg-gradient-to-br from-purple-400 to-purple-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2 text-center leading-5 rounded-2xl">
+              Try Now
+            </button>
+          </Link>
+          <Link href="/github">
+            <button className="text-white bg-gradient-to-br from-purple-400 to-purple-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2 text-center leading-5 rounded-2xl">
+              GitHub
+            </button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
